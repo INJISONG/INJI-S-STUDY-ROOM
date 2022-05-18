96575cba65e1f25515ce9c3f95f8cf71cff5f50f
INJI <105399173+INJISONG@users.noreply.github.com>
2022-05-18 19:37:13 +0900

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,30 @@
+//한줄주석
+/* 여
+러
+줄
+주석 */
+
+let user=50;
+let userTelnuber;
+let userAddress
+
+let num=50;
+let str='자바스크립트';
+let num1='2022';
+let value1=2022+'3030';/* NAN + 는 연결 연산자 */
+console.log(typeof value1);
+console.log(typeof value2);
+
+let str1 = 2022;
+let str2 = '안녕'+str1+'\n년';
+/* 템플릿 리터럴 표기법 백틱 ` */
+let str3 = `안녕하세요 올해는 ${str1} 년 입니다~! `
+console.log(str3)
+
+let isOn=true;
+let isOff=false;
+console.log(isOn);
+console.log(typeof isOff);
+
+let js1;/* 선언 js1이라는 변수다  */
+console.log(typeof js1);/* 값은 undefined */
---
+++ js2.js
@@ -0,0 +1,15 @@
+/* 자바스크립트에서 거짓으로 취급하는 값
+0 -0 null undefinded NaN ' '(빈 문자형) */
+
+/* 오브젝트 객체 */
+let userName;
+let userAge;
+let userTelnuber;
+let user = {
+    name:'king',
+    age: 29,
+    address: 'seoul',
+    tel: '010'
+}
+console.log(user)
+console.log(user.name)
